ebcdfdb5ecbe8e9a39759e3fcfb6aed43792b663
Monjur-Ahmed <52495005+Monjur-Ahmed@users.noreply.github.com>
2022-02-06 23:46:28 +0600

    form updated

--- login.php
+++ login.php
@@ -1,15 +1,27 @@
+<?php 
+
+require 'db.php';
+session_start();
+
+if ($_SESSION['loggedin']) {
+    header("Location: profile.php");
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title>City University of Bangladesh</title>
-
+    <link rel="icon" type="image/png" sizes="32x32" href="img/ICON.png">
     <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap/dataTables.bootstrap.css">
     <link rel="stylesheet" href="bootstrap/AdminLTE.min.css">
     <link rel="stylesheet" href="bootstrap/jquery-ui.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/ICON.png">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <style type="text/css">
         
 .login{ 
@@ -22,7 +34,7 @@
 </head>
 <body>
 
-<?php  include 'header.php'      ?>
+<?php  include ('header.php');      ?>
 
     
 <div class="login">
@@ -30,16 +42,16 @@
 
             <h3 style="text-align: center; padding-top:20px">Login</h3>
             <hr />
-            <form style="width: 50%; margin: 5% 20% 20% 25% ; display: block;">
+            <form style="width: 50%; margin: 5% 20% 20% 25% ; display: block;" method="POST">
               <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
               </div>
               <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
               </div>
-              <button type="submit" class="btn btn-primary">Login</button>
+              <button type="submit" class="btn btn-primary" name="submit">Login</button>
               <hr />
               <button type="button" class="btn btn-link"> <a href="register.php">Register</a> </button>
               <button type="button" class="btn btn-link">Reset Password</button>
@@ -51,7 +63,7 @@
 </div>
 
 
-<?php  include 'footer.php' ?>
+<?php  include ('footer.php'); ?>
 
 
     <script src="js/jQuery-2.1.4.min.js"></script>
@@ -67,3 +79,26 @@
 </body>
 </html>
 
+
+
+<?php
+
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = sha1($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($connect, $sql);
+	if ($result->num_rows > 0) {
+		$_SESSION['loggedin'] = true;
+	  echo " <script> location.replace('profile.php'); </script> ";
+	} else {
+		echo "<script>  swal('Password not matched!', 'Try again', 'error')</script>";
+	}
+}
+
+
+
+
+?>
